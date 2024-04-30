0790f6161d7ba681ab83262fa6102d30da603e7e
Rafael Bonaldi <rafael.simoes@supermix.com.br>
2024-04-30 17:41:33 -0300

    novo projeto

---
+++ Dapper.Infrastructure/Repository/ProductRepository.cs
@@ -0,0 +1,77 @@
+﻿using Dapper.Application.Interfaces;
+using Dapper.Core.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.Infrastructure.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IConfiguration configuration;
+        public ProductRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public async Task<int> AddAsync(Product entity)
+        {
+            entity.CreatedOn = DateTime.Now;
+            var sql = "Insert into Products (Name,Description,Barcode,Rate,CreatedOnx) VALUES (@Name,@Description,@Barcode,@Rate,@CreatedOn)";
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(sql, entity);
+                return result;
+            }
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            var sql = "DELETE FROM Products WHERE Id = @Id";
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(sql, new { Identificador = id });
+                return result;
+            }
+        }
+
+        public async Task<IReadOnlyList<Product>> GetAllAsync()
+        {
+            var sql = "SELECT * FROM Product";
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Product>(sql);
+                return result.ToList();
+            }
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            var sql = "SELECT * FROM Products WHERE Id = @Id";
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QuerySingleOrDefaultAsync<Product>(sql, new { Id = id });
+                return result;
+            }
+        }
+
+        public async Task<int> UpdateAsync(Product entity)
+        {
+            entity.ModifiedOn = DateTime.Now;
+            var sql = "UPDATE Products SET Name = @Name, Description = @Description, Barcode = @Barcode, Rate = @Rate, ModifiedOn = @ModifiedOn  WHERE Id = @Id";
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(sql, entity);
+                return result;
+            }
+        }
+    }
+}
